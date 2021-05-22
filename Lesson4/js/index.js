4c63d31b0f27ea74894940e9b40ee32249f559c0
Resourceman1 <jcdahle02@gmail.com>
2021-05-21 23:02:27 -0600

    Completed majority of lesson 4 and 5

---
+++ Lesson4/js/index.js
@@ -0,0 +1,45 @@
+const menu = document.getElementById('menu');
+
+function toggleMenu() {
+    if (menu.classList.contains('open'))
+        menu.classList.remove('open');
+    else
+        menu.classList.add('open')
+}
+
+(function() {
+    const clock = document.getElementById('clock');
+    const setTime = () => {
+        let d = new Date();
+        const dow = [
+            'Monday', 
+            'Tuesday', 
+            'Wednesday', 
+            'Thursday', 
+            'Friday', 
+            'Saturday', 
+            'Sunday'
+        ];
+        const moy = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'november',
+            'December'
+        ]
+        const day = dow[d.getDay() - 1];
+        const month = moy[d.getMonth()];
+
+        clock.innerHTML = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} - ${day}, ${d.getDate()} ${month} ${d.getFullYear()}`;
+    }
+
+    setInterval(setTime, 1000);
+    setTime();
+})();
