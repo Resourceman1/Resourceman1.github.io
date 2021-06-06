7590bb4492cbe1cb691f141e50b6c67154f00ba0
Resourceman1 <jcdahle02@gmail.com>
2021-06-05 22:33:54 -0600

    Lesson 7 done, except for proper column numbers

--- Lesson6/js/windChill.js
+++ Lesson6/js/windChill.js
@@ -1,4 +1,4 @@
-
+const output = document.getElementById('outputDiv');
 
 
 function fetchVal(id) {
@@ -8,8 +8,8 @@ function fetchVal(id) {
 
 function windChillCalc() {
     var speed = parseFloat(fetchVal('windSpeed'));
-    var temp = parsefloat(fetchVal('temperature'));
-    const output = document.getElementById('outputDiv');
+    var temp = parseFloat(fetchVal('temperature'));
+    
     const windChill = 35.74 + 0.6215 - (35.75 * Math.pow(speed, 0.16)) + 0.4275 * temp * Math.pow(speed, 0.16)
     if ((temp >= 50) && (speed >= 3)) {
         output.innerHTML = "Windchill: " + windChill;
